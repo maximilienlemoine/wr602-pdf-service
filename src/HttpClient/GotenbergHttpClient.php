fe24efb46ff98dfa22a15b6efef5a60b97997b47
Maximilien <maximilien.lemoine.pro@gmail.com>
2024-05-22 12:13:11 +0200

    feat : Pdf from url & htlm file

---
+++ src/HttpClient/GotenbergHttpClient.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\HttpClient;
+
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class GotenbergHttpClient
+{
+    private HttpClientInterface $client;
+    private string $gotenbergUrl;
+
+    public function __construct(HttpClientInterface $client, string $gotenbergUrl)
+    {
+        $this->client = $client;
+        $this->gotenbergUrl = $gotenbergUrl;
+    }
+
+    /**
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ClientExceptionInterface
+     */
+    public function post(string $path, array $options = []): string
+    {
+        $response = $this->client->request('POST', $this->gotenbergUrl . $path, $options);
+
+        return $response->getContent();
+    }
+
+    /**
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ClientExceptionInterface
+     */
+    public function get(string $path, array $options = []): string
+    {
+        $response = $this->client->request('GET', $this->gotenbergUrl . $path, $options);
+
+        return $response->getContent();
+    }
+
+}
